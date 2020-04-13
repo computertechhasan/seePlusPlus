320522abb41cd8d50c889c882a33b10fe3d626f4
Hasan <hasan_skaterboy@hotmail.com>
2020-04-12 20:53:17 -0400

    Got the ping project working!

--- PingProject/server.cpp
+++ PingProject/server.cpp
@@ -8,14 +8,35 @@
 
 int main() {
     int PORT = 3000;
-    int dataLen = 1024;
-    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket == -1) {
+    int data_len = 1024;
+    int bytes_received;
+    unsigned char buf[data_len];
+
+    // create socket and get fd
+    int server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket == -1) {
         std::cout << "There was an error creating the socket!" << std::endl;
+        return 0;
     }
-
-
-
-
-
+    struct sockaddr_in server_address;
+    struct sockaddr_in remote_address;
+    socklen_t address_length = sizeof(remote_address);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+    // bind socket
+    int bound_socket = bind(server_socket, (struct sockaddr *)&server_address, address_length);
+    if (bound_socket == -1) {
+        std::cout << "There was an error binding the socket!" << std::endl;
+        return 0;
+    }
+    // Listen for messages 
+    while (true) {
+        std::cout << "Listening on port:" << PORT << std::endl;
+        bytes_received = recvfrom(server_socket, buf, data_len, 0, (struct sockaddr *)&remote_address, &address_length);
+        if (bytes_received > 0) {
+            buf[bytes_received] = 0;
+            std::cout << "Got message:" << buf << std::endl;
+        }
+    }
 }
