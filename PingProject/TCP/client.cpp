f4d1ed49a090bffd252714543b1e4ccaa23a5080
Hasan <hasan_skaterboy@hotmail.com>
2020-04-13 20:43:49 -0400

    Got TCP working!

---
+++ PingProject/TCP/client.cpp
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <iostream>
+
+int main() {
+    int data_len = 1024;
+    int PORT = 3000;
+    char* message = "I worked! Now lets try something really long to see if it still works cuz there is a limit to this";
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
+
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        std::cout << "There was an error creating the socket!" << std::endl;
+        return 0;
+    }
+
+    int connected_socket = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (connected_socket == -1) {
+        std::cout << "There was an error connecting to the server!" << std::endl;
+        return 0;
+    }
+    int send_message = send(client_socket, message, strlen(message), 0);
+
+    if (send_message == -1) {
+        std::cout << "There was an error sending!" << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "Client sent the message:" << message << std::endl;
+        return 0;
+    }
+}
---
+++ PingProject/TCP/server.cpp
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+
+int main() {
+    int PORT = 3000;
+    int data_len = 1024;
+    int bytes_received;
+    unsigned char buf[data_len];
+
+    // create socket and get fd
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        std::cout << "There was an error creating the socket!" << std::endl;
+        return 0;
+    }
+    struct sockaddr_in server_address;
+    struct sockaddr_in remote_address;
+    socklen_t address_length = sizeof(remote_address);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+    // bind socket
+    int bound_socket = bind(server_socket, (struct sockaddr *)&server_address, address_length);
+    if (bound_socket == -1) {
+        std::cout << "There was an error binding the socket!" << std::endl;
+        return 0;
+    }
+    // Listen for messages
+    std::cout << "Listening on port:" << PORT << std::endl;
+    int listening_socket = listen(server_socket, 5);
+    if (listening_socket == -1) {
+        std::cout << "There was an error making the socket listen!" << std::endl;
+        return 0;
+    }
+   
+    while (true) {
+        int read_socket = accept(server_socket, (struct sockaddr *)&remote_address, &address_length);
+        if (read_socket < 0) {
+            std::cout << "There was a handshake error!" << std::endl;
+            return 0;
+        }
+        // bytes_received = recvfrom(server_socket, buf, data_len, 0, (struct sockaddr *)&remote_address, &address_length);
+        bytes_received = read(read_socket, buf, data_len);
+        if (bytes_received > 0) {
+            buf[bytes_received] = 0;
+            std::cout << "Got message:" << buf << std::endl;
+        }
+    }
+}
